96e2c5adee77c6321675828df179fcac9439ea58
Ayush-00010 <146408867+Ayush-00010@users.noreply.github.com>
2024-06-19 14:24:48 +0530

    Update Login.jsx

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://jobzee-backend-d7ku.onrender.com/api/v1/user/login",
+        "https://job-zee-frontend-sigma.vercel.app/api/v1/user/login",
         { email, password, role },
         {
           headers: {
