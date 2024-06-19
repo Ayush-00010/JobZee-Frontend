b7d0ec831fb1275ca06930811989e05852506c92
Ayush-00010 <146408867+Ayush-00010@users.noreply.github.com>
2024-06-19 12:28:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ const App = () => {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="https://jobzee-backend-d7ku.onrender.com/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/job/getall" element={<Jobs />} />
           <Route path="/job/:id" element={<JobDetails />} />
           <Route path="/application/:id" element={<Application />} />
