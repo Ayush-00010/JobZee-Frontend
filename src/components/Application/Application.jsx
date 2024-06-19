521158025e0a95dc3103855decb55592e10fd7b6
Ayush-00010 <146408867+Ayush-00010@users.noreply.github.com>
2024-06-19 11:25:15 +0530

    Update Application.jsx

--- src/components/Application/Application.jsx
+++ src/components/Application/Application.jsx
@@ -35,7 +35,7 @@ const Application = () => {
 
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/application/post",
+        "https://jobzee-backend-d7ku.onrender.com/api/v1/application/post",
         formData,
         {
           withCredentials: true,
